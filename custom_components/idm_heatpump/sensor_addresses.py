fd86d2eaeef7b37958bd029f02fa3e0cecea3423
Klemens Böswirth <k.boeswirth+git@gmail.com>
2023-10-29 18:27:35 +0000

    remove min value for power_solar_surplus
    A negative value could be interpreted as "consuming more than producing" (i.e. deficit). Whether that happens is unclear, but better to keep the option open.

--- custom_components/idm_heatpump/sensor_addresses.py
+++ custom_components/idm_heatpump/sensor_addresses.py
@@ -1106,7 +1106,6 @@ SENSOR_ADDRESSES: dict[str, IdmSensorAddress] = {
             unit=POWER_KILO_WATT,
             device_class=SensorDeviceClass.POWER,
             state_class=SensorStateClass.MEASUREMENT,
-            min_value=0,
             supported_features=SensorFeatures.SET_POWER,
         ),
         _FloatSensorAddress(
